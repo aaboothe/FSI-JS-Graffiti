a07950d65ae5a545b763c8311494745e460482ae
aaboothe <aaboothe73@gmail.com>
2021-11-29 17:57:55 -0800

    final

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left";
+header.style.fontFamily = "sans-serif";
+header.style.backgroundColor = 'blue';
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderStyle = 'dashed';
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
+
+let column = document.querySelectorAll('.column')
+for(let c = 0; c < column.length; c++){
+    column[c].style.backgroundColor = 'grey'
+}
+
+let dogname = document.querySelectorAll('.dog-name')
+for(let n = 0; n < dogname.length; n++){
+    dogname[n].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'green';
+footer.style.fontSize = '50px';
 
 // Select some elements...
